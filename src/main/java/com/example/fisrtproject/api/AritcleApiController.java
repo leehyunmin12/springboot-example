550cf3933d3cb7892ca7101690de6ef7d7906a95
leehyunmin <leehm2292@gmail.com>
2025-08-06 13:04:53 +0900

    fix: add service

--- src/main/java/com/example/fisrtproject/api/AritcleApiController.java
+++ src/main/java/com/example/fisrtproject/api/AritcleApiController.java
@@ -2,7 +2,7 @@ package com.example.fisrtproject.api;
 
 import com.example.fisrtproject.dto.ArticleForm;
 import com.example.fisrtproject.entity.Article;
-import com.example.fisrtproject.repository.ArticleRepository;
+import com.example.fisrtproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,47 +15,38 @@ import java.util.List;
 @RestController
 public class AritcleApiController {
     @Autowired
-    private ArticleRepository articleRepository;
+    private ArticleService articleService;
     //GET
     @GetMapping("/api/articles")
     public List<Article> index() {
-        return articleRepository.findAll();
+        return articleService.index();
     }
     @GetMapping("/api/articles/{id}")
     public Article show(@PathVariable Long id){
-        return articleRepository.findById(id).orElse(null);
+        return articleService.show(id);
     }
     //POST
     @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto){
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto){
+        Article created = articleService.create(dto);
+        return (created!=null)?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     //PATCH
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto){
-        Article article = dto.toEntity();
-        log.info("id: {}, article: {}", id, article.toString());
-        Article target = articleRepository.findById(id).orElse(null);
-        if(target==null || id!= article.getId()) {
-            log.info("잘못된 요청! id: {}, article: {}",id,article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Article updated = articleService.update(id, dto);
+        return (updated!=null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     //DELETE
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id) {
-        //1. 대상 찾기
-        Article target = articleRepository.findById(id).orElse(null);
-        //2. 잘못된 요청 처리하기
-        if(target == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        //3. 대상 삭제하기
-        articleRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        Article deleted = articleService.delete(id);
+        return (deleted!=null) ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
---
+++ src/main/java/com/example/fisrtproject/service/ArticleService.java
@@ -0,0 +1,58 @@
+package com.example.fisrtproject.service;
+
+import com.example.fisrtproject.dto.ArticleForm;
+import com.example.fisrtproject.entity.Article;
+import com.example.fisrtproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class ArticleService {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    public Article show(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+        if(article.getId() != null){
+            return null;
+        }
+        return articleRepository.save(article);
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        Article target = articleRepository.findById(id).orElse(null);
+        if(target==null || id!= article.getId()) {
+            log.info("잘못된 요청! id: {}, article: {}",id,article.toString());
+            return null;
+        }
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
+    }
+
+    public Article delete(Long id) {
+        //1. 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+        //2. 잘못된 요청 처리하기
+        if(target == null) {
+            return null;
+        }
+        //3. 대상 삭제하기
+        articleRepository.delete(target);
+        return target;
+    }
+}
